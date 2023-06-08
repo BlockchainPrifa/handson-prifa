328aabd22447edb4cc52d8b9fc0cfa12005c9270
BlockchainPrifa <blockchain.prifa@gmail.com>
2023-06-08 17:25:41 +0800

    complete handson eVoting

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
 const { createApp } = Vue;
-console.log(ABI);
+
 createApp({
     data() {
         return {
             message: "Hello Vue!",
-            contractAddress: "0xdacffdC47269310CB7fEAa16274A1d426f5D1771",
+            contractAddress: "0x6aebD9716a012956F3BaB4e70099E462A5407B63",
             count: 0,
             token: null,
             newVoter: "",
@@ -16,9 +16,6 @@ createApp({
         };
     },
     methods: {
-        increment() {
-            this.count++;
-        },
         async _initEthers() {
             const [selectedAddress] = await window.ethereum.enable();
             // ethers connection for the smartcontract
@@ -29,7 +26,6 @@ createApp({
                 ABI,
                 _provider.getSigner()
             );
-            const data = 20;
             // get the proposals
             const newProposal = await _token.getAllProposals();
             console.log(newProposal);
@@ -42,17 +38,19 @@ createApp({
             this.token = _token;
             this.proposals = newProposal;
             this.chairperson = newChairperson;
-            this.proposals.map(item => {
-                console.log(item[1].toNumber())
-            })
+            this.proposals.map((item) => {
+                console.log(item[1].toNumber());
+            });
         },
         async voteProposal(proposal) {
             await this.token.vote(proposal);
         },
         async checkAddressVoter() {
             try {
-                const voterData = await this.token.voters(`${voterAddressToCheck}`);
+                const voterData = await this.token.voters(this.voterAddressToCheck);
+                console.log(voterData)
                 this.voterStatus = voterData;
+                console.log(this.voterStatus)
             } catch (err) {
                 console.log(err);
                 this.voterStatus = "An error has occured";
@@ -60,18 +58,22 @@ createApp({
         },
         async addNewVoter() {
             try {
-                await this.token.giveRightToVote(newVoter);
-                this.voterStatus = "Success";
+                await this.token.giveRightToVote(this.newVoter);
             } catch (err) {
                 console.log(err);
-                this.voterStatus = "An error has occured";
             }
         },
+        async reload() {
+            this._initEthers()
+        },
     },
-    computed: {},
+    computed: {
+        checkVote() {
+            // `this` points to the component instance
+            return this.voterStatus[1] ? 'sudah' : 'belum'
+        }
+    },
     mounted() {
-        // methods can be called in lifecycle hooks, or other methods!
-        this.increment();
         this._initEthers();
     },
 }).mount("#app");
